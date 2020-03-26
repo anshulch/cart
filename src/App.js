e584134dbfc4e342319b1ce283424899e1c01231
Anshul <anshul.chaurasia7@gmail.com>
2020-03-26 13:35:06 +0530

    added font-awesome icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+// import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <FontAwesomeIcon icon={faCoffee} />
         <img src={logo} className="App-logo" alt="logo" />
+        {/* {element} */}
         <p>
           Edit <code>src/App.js</code> Anshul chaurasia and save to reload.....
         </p>
